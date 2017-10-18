d8af8b0c82df41dacb4acaf2766729c722ca2629
Eric Poon <ericpoon510@gmail.com>
2017-10-19 00:53:41 +0800

    Improved card layout

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,14 +1,79 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image, Linking} from 'react-native';
 import Card from './Card';
+import CardSection from './CardSection';
+import Button from './Button';
 
 // every time add a new component,
 // consider it's functional component (static) or class component (dynamic)
 
-const AlbumDetail = (props) => (
-    <Card>
-        <Text>{props.album.title}</Text>
-    </Card>
-);
+const AlbumDetail = ({album}) => {
+    const {
+        title,
+        artist,
+        image,
+        url
+    } = album;
+    const thumbnailImage = album['thumbnail_image'];
+    const {
+        headerContentStyle,
+        headerTextStyle,
+        thumbnailStyle,
+        thumbnailContainerStyle,
+        imageStyle,
+    } = styles;
+
+    return (
+        <Card>
+            <CardSection>
+                <View style={thumbnailContainerStyle}>
+                    <Image
+                        style={thumbnailStyle}
+                        source={{uri: thumbnailImage}}
+                    />
+                </View>
+                <View style={headerContentStyle}>
+                    <Text style={headerTextStyle}>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
+            </CardSection>
+            <CardSection>
+                <Image
+                    style={imageStyle}
+                    source={{uri: image}}
+                />
+            </CardSection>
+            <CardSection>
+                <Button onPress={() => Linking.openURL(url)}>
+                    Buy now
+                </Button>
+            </CardSection>
+        </Card>
+    );
+};
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+    },
+    headerTextStyle: {
+        fontSize: 18,
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50,
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginHorizontal: 10,
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null,
+    }
+};
 
 export default AlbumDetail;
